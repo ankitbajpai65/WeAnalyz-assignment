2115596b16143f40fc626635c8f4e2537a14a71c
Ankit Bajpai <ankit65bajpai@gmail.com>
2023-03-15 23:20:32 +0530

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
-import Footer from './Components/Footer';
-import Signin from './Components/Signin';
 
 function App() {
   return (
